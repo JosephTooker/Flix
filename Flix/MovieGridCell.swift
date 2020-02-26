801f10b60ee95b49fa3dcba53038f4175b18ca12
Joseph Tooker <tookerjt@gmail.com>
2020-02-25 23:35:17 -0500

    Added the ability to view current movie details, and added the superhero movie tab

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Joseph Tooker on 2/25/20.
+//  Copyright © 2020 Joseph Tooker. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
